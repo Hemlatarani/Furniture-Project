4369c426fc1fb84739d742c0ad59d2f79c22b3db
Hemlata Rani <ranirani81996@gmail.com>
2026-08-23 23:30:38 +0530

    breadcrumb fixed

--- client/furniture/src/app/(withheader&Footer)/DorianShoe-us/page.js
+++ client/furniture/src/app/(withheader&Footer)/DorianShoe-us/page.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import Breadcrumb from '../common/Breadcrumb'
 import DorianContent from './DorianContent'
 import DorianProduct from './DorianProduct'
 import DorianUpsells from './DorianUpsells'
+import Breadcrumb from '@/app/common/Breadcrumb'
 
 export default function DorianShoe() {
-    let pageName="Dorian Shoe Rack"
+  let pageName = "Dorian Shoe Rack"
   return (
     <>
-    <Breadcrumb pageName={pageName}/>
-    <DorianContent/>
-  <DorianProduct/>
-    <DorianUpsells/>
-   
+      <Breadcrumb pageName={pageName} />
+      <DorianContent />
+      <DorianProduct />
+      <DorianUpsells />
+
     </>
   )
 }
