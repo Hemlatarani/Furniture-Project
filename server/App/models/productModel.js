303f8bbdfd779729b093de25eb49cc8c181250d9
Hemlata Rani <ranirani81996@gmail.com>
2026-09-04 12:11:28 +0530

    fix

--- server/App/models/productModel.js
+++ server/App/models/productModel.js
@@ -1,66 +1,155 @@
 const { populate } = require("dotenv")
-let mongoose = require("mongoose")
-let productSchema = mongoose.Schema(
-    {
-        productName: {
-            type: String,
-            required: true,
-            minlength: 2,
-            maxlength: 30,
-            // unique: true,
-        },
+// let mongoose = require("mongoose")
+// let productSchema = mongoose.Schema(
+//     {
+//         productName: {
+//             type: String,
+//             required: true,
+//             minlength: 2,
+//             maxlength: 30,
+//             // unique: true,
+//         },
 
-        parentCategory: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "category"
-        },
-        subCategoryData: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "subcategory"
-        },
-        subsubCategoryData: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "subsubcategory"
-        },
-        productMaterial: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "material"
-        }],
-        productColor: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "color"
-        }],
-        productType: {
-            type:String,
-            enum:['Featured','New Arrivals','Onsale','Best Selling','Premium','Exclusive','Hot Deal','Flash Sale']
-            //enum is a special data type in programming that creates a set of related, named constants
-        },
-        productGallery:Object,
-        backImage:String,
-        isSelling:{
-            type: Boolean,
-            default: false
-        } ,
-        topRated:{
-            type: Boolean,
-            default: false
-        } , 
-        isUpsell:{
-            type: Boolean,
-            default: false
-        } , 
-        actulPrice: Number,
-        salePrice: Number,
-        isStock:Number,
-        productImage: String,
-        productOrder: Number,
-        productDesc: String,
-        slug:String,
-        productStatus: {
-            type: Boolean,
-            default: true
-        }
+//         parentCategory: {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "category"
+//         },
+//         subCategoryData: {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "subcategory"
+//         },
+//         subsubCategoryData: {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "subsubcategory"
+//         },
+//         productMaterial: [{
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "material"
+//         }],
+//         productColor: [{
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "color"
+//         }],
+//         productType: {
+//             type:String,
+//             enum:['Featured','New Arrivals','Onsale','Best Selling','Premium','Exclusive','Hot Deal','Flash Sale']
+//             //enum is a special data type in programming that creates a set of related, named constants
+//         },
+//         productGallery:Object,
+//         backImage:String,
+//         isSelling:{
+//             type: Boolean,
+//             default: false
+//         } ,
+//         topRated:{
+//             type: Boolean,
+//             default: false
+//         } , 
+//         isUpsell:{
+//             type: Boolean,
+//             default: false
+//         } , 
+//         actulPrice: Number,
+//         salePrice: Number,
+//         isStock:Number,
+//         productImage: String,
+//         productOrder: Number,
+//         productDesc: String,
+//         slug:String,
+//         productStatus: {
+//             type: Boolean,
+//             default: true
+//         }
+//     }
+// )
+// let productModel = mongoose.model("product", productSchema)
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    productName: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 30,
+    },
+
+    parentCategory: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "category"
+    },
+
+    subCategoryData: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "subcategory"
+    },
+
+    subsubCategoryData: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "subsubcategory"
+    },
+
+    productMaterial: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "material"
+    }],
+
+    productColor: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "color"
+    }],
+
+    productType: {
+        type: String,
+        enum: [
+            "Featured",
+            "New Arrivals",
+            "Onsale",
+            "Best Selling",
+            "Premium",
+            "Exclusive",
+            "Hot Deal",
+            "Flash Sale"
+        ]
+    },
+
+    productGallery: Object,
+
+    backImage: String,
+
+    isSelling: {
+        type: Boolean,
+        default: false
+    },
+
+    topRated: {
+        type: Boolean,
+        default: false
+    },
+
+    isUpsell: {
+        type: Boolean,
+        default: false
+    },
+
+    actulPrice: Number,
+
+    salePrice: Number,
+
+    isStock: Number,
+
+    productImage: String,
+
+    productOrder: Number,
+
+    productDesc: String,
+
+    slug: String,
+
+    productStatus: {
+        type: Boolean,
+        default: true
     }
-)
-let productModel = mongoose.model("product", productSchema)
+});
+
+const productModel = mongoose.model("product", productSchema);
 module.exports = { productModel }
