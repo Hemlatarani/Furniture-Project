93f1175e627c44612db56cbc3094a53dba7be753
Hemlata Rani <ranirani81996@gmail.com>
2026-09-10 11:55:05 +0530

    serve error

--- server/App/models/companyAccountModel.js
+++ server/App/models/companyAccountModel.js
@@ -1,51 +1,61 @@
-let mongoose =require("mongoose")
+let mongoose = require("mongoose");
 
-let companyAccountSchema= new mongoose.Schema({
-    Name:{
-        type:String,
-        required:true,
+let companyAccountSchema = new mongoose.Schema({
+    Name: {
+        type: String,
+        required: true,
     },
-    Email:{
-        type:String,
-        required:true,
-        unique:true
+
+    Email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    Mobilenumber:{
-        type:String,
-        required:true
+
+    Mobilenumber: {
+        type: String,
+        required: true
     },
-    Address:{
-        type:String,
-        required:true
+
+    Address: {
+        type: String,
+        required: true
     },
-    MapUrl:{
-        type:String,
-        // required:true,
+
+    MapUrl: {
+        type: String,
     },
-    imageLogo:{
-        type:String,
-        required:true,
+
+    imageLogo: {
+        type: String,
+        required: true,
     },
-    Facebook:{
-        type:String,
-        // required:true
+
+    Facebook: {
+        type: String,
     },
-     Youtube:{
-        type:String,
-        // required:true
+
+    Youtube: {
+        type: String,
     },
-    Twitter:{
-        type:String,
-        // required:true
+
+    Twitter: {
+        type: String,
     },
-    Instagram:{
-        type:String,
-        // required:true
+
+    Instagram: {
+        type: String,
     },
-whatsapp:{
-    type:String,
-    // required:true
-},
-})
-let companyAccountSchemaModel=mongoose.model("companyAccount",companyAccountSchema)
-module.exports={companyAccountSchemaModel}
+
+    whatsapp: {
+        type: String,
+    },
+});
+
+let companyAccountSchemaModel = mongoose.model(
+    "companyAccount",
+    companyAccountSchema,
+    "companyaccount"
+);
+
+module.exports = { companyAccountSchemaModel };
